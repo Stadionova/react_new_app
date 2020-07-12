220ad07046a91a2f5136f1e4498215924c0a6260
Yulia Tribunskaya <ytribunskaya@kameleoon.com>
2020-07-12 21:03:49 +0300

    deleted all test components to start new social network from the beginning

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import './App.css';
-import LiElements from './LiElements.js';
-import Header from './Header.js';
 
 const App = () => {
   return (
     <div className="App">
-        first component return simple HTML
-        <li>FIRST COMPONENT HEADER</li>
-        <Header />
-        <LiElements />
     </div>
   );
 }
