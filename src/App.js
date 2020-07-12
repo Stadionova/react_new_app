c97f2472e40b0c1a59f99dee22c934b2f290d2b8
Yulia Tribunskaya <ytribunskaya@kameleoon.com>
2020-07-12 22:05:36 +0300

    colored all children & text

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import "./App.css";
 const App = () => {
   return (
     <div className="app-wrapper">
-      <header className="app-wrapper__header"></header>
-      <nav className="app-wrapper__navigation"></nav>
-      <main className="app-wrapper__main-content"></main>
+      <header className="app-wrapper__header">HEADER</header>
+      <nav className="app-wrapper__navigation">SIDEBAR</nav>
+      <main className="app-wrapper__main-content">MAIN CONTENT</main>
     </div>
   );
 };
