31eef9ca37d9afb00abc15e4d735bd13ebf071e1
Yulia Tribunskaya <ytribunskaya@kameleoon.com>
2020-07-12 12:53:31 +0300

    created one more Header component & put it into first App component

--- src/App.js
+++ src/App.js
@@ -4,8 +4,18 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-        function return simple HTML
-        <li>tag LI</li>
+        first component return simple HTML
+        <li>FIRST COMPONENT HEADER</li>
+        <li><Header /></li>
+    </div>
+  );
+}
+
+const Header = () => {
+  return (
+    <div className="App">
+        second component Header put into first App component
+        <li>SECOND COMPONENT HEADER</li>
     </div>
   );
 }
