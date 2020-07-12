37e7de97b23f045bb44f55b0f38f23b683bcc1c0
Yulia Tribunskaya <ytribunskaya@kameleoon.com>
2020-07-12 11:25:59 +0300

    added new text in html

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
+        function return simple HTML
     </div>
   );
 }
