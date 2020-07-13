6981925136727981bb8a0d0399ed544c9061df92
Yulia Tribunskaya <ytribunskaya@kameleoon.com>
2020-07-13 20:24:27 +0300

    created components for every child

--- src/App.js
+++ src/App.js
@@ -1,50 +1,15 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header";
+import Navigation from "./components/Navigation";
+import MainContent from "./components/MainContent";
 
 const App = () => {
   return (
     <div className="app-wrapper">
-      <header className="app-wrapper__header">
-        <img src="https://static.rfstat.com/renderforest/images/v2/logo-homepage/line_style_2.png"></img>
-      </header>
-      <nav className="app-wrapper__navigation">
-        <div>
-          <a>Profile</a>
-        </div>
-        <div>
-          <a>Messages</a>
-        </div>
-        <div>
-          <a>News</a>
-        </div>
-        <div>
-          <a>Music</a>
-        </div>
-        <div>
-          <a>Settings</a>
-        </div>
-      </nav>
-      <main className="app-wrapper__main-content">
-        <div className="app-wrapper__main-content-headlinePhoto">
-          <img src="https://news.liga.net/images/general/2019/09/11/20190911155211-5324.jpg?v=1568211567"></img>
-        </div>
-        <div className="app-wrapper__main-content-avatarAndDesc">
-          <div>
-            <img src="https://99px.ru/sstorage/56/2019/09/mid_324597_239159.jpg"></img>
-          </div>
-          <div>Profile Description</div>
-        </div>
-        <div className="app-wrapper__main-content-posts">
-          <div>My posts</div>
-          <div>Create New Post</div>
-          <div>
-            <input></input>
-          </div>
-          <div>
-            <button>Button</button>
-          </div>
-        </div>
-      </main>
+      <Header />
+      <Navigation />
+      <MainContent />
     </div>
   );
 };
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <header className="app-wrapper__header">
+            <img src="https://static.rfstat.com/renderforest/images/v2/logo-homepage/line_style_2.png"></img>
+        </header>
+    )
+};
+
+export default Header;
