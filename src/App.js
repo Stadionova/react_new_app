faa438eef4909c066d1f2978feda1aee112163df
Yulia Tribunskaya <ytribunskaya@kameleoon.com>
2020-07-13 09:55:28 +0300

    added content to the side bar & logo to the header

--- src/App.js
+++ src/App.js
@@ -4,8 +4,26 @@ import "./App.css";
 const App = () => {
   return (
     <div className="app-wrapper">
-      <header className="app-wrapper__header">HEADER</header>
-      <nav className="app-wrapper__navigation">SIDEBAR</nav>
+      <header className="app-wrapper__header">
+        <img src="https://static.rfstat.com/renderforest/images/v2/logo-homepage/line_style_2.png"></img>
+      </header>
+      <nav className="app-wrapper__navigation">
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <div>
+          <a>Settings</a>
+        </div>
+      </nav>
       <main className="app-wrapper__main-content">MAIN CONTENT</main>
     </div>
   );
