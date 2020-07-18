fab6bbaeb38714784cfdf733c88e6c29e2e94c06
Yulia Tribunskaya <ytribunskaya@kameleoon.com>
2020-07-18 22:13:09 +0300

    added my own active class to NavLink component instead of default active class as I can add my own css styles now

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -6,19 +6,19 @@ const Navigation = () => {
     return (
         <nav className={classes.navigation}>
             <div className={classes.item}>
-                <NavLink to='/profile'>Profile</NavLink>
+                <NavLink to='/profile' activeClassName={classes.activeLink}>Profile</NavLink>
             </div>
             <div className={classes.item}>
-                <NavLink to='/messages'>Messages</NavLink>
+                <NavLink to='/messages' activeClassName={classes.activeLink}>Messages</NavLink>
             </div>
             <div className={classes.item}>
-                <NavLink to='/news'>News</NavLink>
+                <NavLink to='/news' activeClassName={classes.activeLink}>News</NavLink>
             </div>
             <div className={classes.item}>
-                <NavLink to='/music'>Music</NavLink>
+                <NavLink to='/music' activeClassName={classes.activeLink}>Music</NavLink>
             </div>
             <div className={classes.item}>
-                <NavLink to='/settings'>Settings</NavLink>
+                <NavLink to='/settings' activeClassName={classes.activeLink}>Settings</NavLink>
             </div>
         </nav>
     )
