ee068272c25d24bc6ac0c47989b7971d6bc1103c
Yulia Tribunskaya <ytribunskaya@kameleoon.com>
2020-07-19 21:14:46 +0300

    cut Profile component to 2 components: MyPosts & ProfileInfo (desc+ava)

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,19 +1,11 @@
 import React from "react";
-import classes from "./Profile.module.css";
 import MyPosts from "./MyPosts/MyPosts";
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 const Profile = () => {
     return (
-        <main className={classes.content}>
-            <div className={classes.headlinePhoto}>
-                <img src="https://news.liga.net/images/general/2019/09/11/20190911155211-5324.jpg?v=1568211567"></img>
-            </div>
-            <div className={classes.avatarAndDesc}>
-                <div>
-                    <img src="https://99px.ru/sstorage/56/2019/09/mid_324597_239159.jpg"></img>
-                </div>
-                <div>Profile Description</div>
-            </div>
+        <main>
+            <ProfileInfo />
             <MyPosts />
         </main>
     )
---
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import classes from "./ProfileInfo.module.css";
+
+const ProfileInfo = () => {
+    return (
+        <div>
+            <div className={classes.headlinePhoto}>
+                <img src="https://news.liga.net/images/general/2019/09/11/20190911155211-5324.jpg?v=1568211567"></img>
+            </div>
+            <div className={classes.avatarAndDesc}>
+                <div>
+                    <img src="https://99px.ru/sstorage/56/2019/09/mid_324597_239159.jpg"></img>
+                </div>
+                <div>Profile Description</div>
+            </div>
+        </div>
+    )
+};
+
+export default ProfileInfo;
