27ab210ea6ea2e9a11fa743323e718c847e0baed
Yulia Tribunskaya <ytribunskaya@kameleoon.com>
2020-07-19 19:59:00 +0300

    added dialogs & messages columns to Dialogs component & fixed some styles of active links in Navigation component

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,30 @@
 import React from "react";
 import classes from "./Dialogs.module.css";
+import { NavLink } from "react-router-dom";
 
 const Dialogs = () => {
     return (
-        <div className={classes.dialogs}>Dialogs Page</div>
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div>
+                    <NavLink to='/anton' className={classes.itemDialog} activeClassName={classes.activeDialog}>Anton</NavLink>
+                </div>
+                <div>
+                    <NavLink to='/sveta' className={classes.itemDialog} activeClassName={classes.activeDialog}>Sveta</NavLink>
+                </div>
+                <div>
+                    <NavLink to='/yulia' className={classes.itemDialog} activeClassName={classes.activeDialog}>Yulia</NavLink>
+                </div>
+                <div>
+                    <NavLink to='/sergey' className={classes.itemDialog} activeClassName={classes.activeDialog}>Sergey</NavLink>
+                </div>
+            </div>
+            <div className={classes.dialogsMessages}>
+                <div className={classes.messages}>Hi!</div>
+                <div className={classes.messages}>How are you doing?</div>
+                <div className={classes.messages}>Don't leave me onread, please...</div>
+            </div>
+        </div>
     )
 };
 
