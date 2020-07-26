b6533226cc68166b259960724b6a761055936b4d
Yulia Tribunskaya <ytribunskaya@kameleoon.com>
2020-07-26 13:50:32 +0300

    put objs with component data to state obj

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,19 +1,15 @@
-const dialogsData = [
-    { id: 1, name: 'Anton', message: 'Hi, how are you doing?' },
-    { id: 2, name: 'Sergey', message: 'Why did you leave me on read?' },
-    { id: 3, name: 'Yulia', message: 'Ok, back off!' },
-    { id: 4, name: 'Sveta', message: 'bye!' }
-];
-
-const messagesData = [
-    { id: 1, name: 'Anton', message: 'Hi, how are you doing?' },
-    { id: 2, name: 'Sergey', message: 'Why did you leave me on read?' },
-    { id: 3, name: 'Yulia', message: 'Ok, back off!' }
-];
-
 let state = {
-    dialogsState: dialogsData,
-    messagesState: messagesData
+    dialogsState: [
+        { id: 1, name: 'Anton', message: 'Hi, how are you doing?' },
+        { id: 2, name: 'Sergey', message: 'Why did you leave me on read?' },
+        { id: 3, name: 'Yulia', message: 'Ok, back off!' },
+        { id: 4, name: 'Sveta', message: 'bye!' }
+    ],
+    messagesState: [
+        { id: 1, name: 'Anton', message: 'Hi, how are you doing?' },
+        { id: 2, name: 'Sergey', message: 'Why did you leave me on read?' },
+        { id: 3, name: 'Yulia', message: 'Ok, back off!' }
+    ]
 }
 
 export default state;
