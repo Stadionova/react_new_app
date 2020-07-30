94789961acc4f76f307d084514d84c2030c2211a
Yulia Tribunskaya <ytribunskaya@kameleoon.com>
2020-07-30 20:33:42 +0300

    added input & button for new messages => used createRef link to get value from input after button click

--- src/components/Dialogs/Messages/Messages.jsx
+++ src/components/Dialogs/Messages/Messages.jsx
@@ -2,7 +2,21 @@ import React from "react";
 import classes from "./Messages.module.css";
 
 const Messages = (props) => {
-    return <div className={classes.messages}>{props.message}</div>
+
+    const newMessage = React.createRef();
+
+    function showNewMessage() {
+        let message = newMessage.current.value;
+        console.log('message ', message);
+    }
+
+    return (
+        <div>
+            <div className={classes.messages}>{props.message}</div>
+            <input ref={newMessage} type="text" />
+            <button onClick={showNewMessage}>Show NEW Message</button>
+        </div>
+    )
 };
 
 export default Messages;
