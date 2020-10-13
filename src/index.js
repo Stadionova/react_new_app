2bad73dc7dc4bedff8b33051942e69520a753a43
Yulia Tribunskaya <ytribunskaya@kameleoon.com>
2020-10-13 21:50:17 +0300

    plugged Provider function from react-redux instead of context

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import store from './redux/redux-store';
-import StoreContext from './StoreContext';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
-let renderFunc = (props) => {
+let renderFunc = () => {
   ReactDOM.render(
     <BrowserRouter>
-      <StoreContext.Provider value={store}>
-        <App props={props} />
-      </StoreContext.Provider>
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>, document.getElementById('root'));
 }
 
