cabf96122445ad58856389019a8cc36641bd5394
Yulia Tribunskaya <ytribunskaya@kameleoon.com>
2020-12-05 21:59:39 +0300

    changed styles of header & icon & title

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,8 +4,8 @@ import classes from "./Header.module.css";
 const Header = () => {
     return (
         <header className={`${classes.header} ${classes.headerSize}`}>
-            HEADER
-            <img src="https://static.rfstat.com/renderforest/images/v2/logo-homepage/line_style_2.png"></img>
+            <img src="https://www.publicdomainpictures.net/pictures/250000/velka/corazon-sobre-fondo-de-madera.jpg"></img>
+            <div><i><span>Heart to Heart talks</span></i></div>
         </header>
     )
 };
