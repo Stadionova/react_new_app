9a6cde1d9d36e36705ce0c9a120de6ddd3988f1c
Yulia Tribunskaya <ytribunskaya@kameleoon.com>
2021-01-04 16:34:23 +0300

    added styles & text for user component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,12 +2,23 @@ import React from "react";
 import classes from "./Users.module.css";
 
 const Users = (props) => {
+    console.log('props ', props);
     return <div>
         {
-            props.users.map(u => { //props.users передаём через функцию mapStateToProps в контейнерной компоненте
-                return <div key={u.id} className={classes.user}>
-                    <span>FOLLOW</span>
-                </div>
+            props.users.map(u => { // props.users передаём через функцию mapStateToProps в контейнерной компоненте
+                return (
+                    <div key={u.id} className={classes.user}>
+                        <button>{u.followed ? 'FOLLOW' : 'UNFOLLOW'}</button>
+                        <div className={classes.nameAndStatus}>
+                            <div><span>{u.name}</span></div>
+                            <div><span>{u.status}</span></div>
+                        </div>
+                        <div className={classes.countryAndCity}>
+                            <div><span>{u.country}</span></div>
+                            <div><span>{u.city}</span></div>
+                        </div>
+                    </div>
+                )
             })
         }
     </div>
